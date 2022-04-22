a363976e34c26ae434c1a427e7a43570a70179e7
jvoas655 <jvoas@utexas.edu>
2022-04-22 15:17:14 -0500

    Adding basic episode loop

--- src/train.py
+++ src/train.py
@@ -10,9 +10,10 @@ if __name__ == "__main__":
     # Arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--num_agents", type=int, default=50)
+    parser.add_argument("--num_episodes", type=int, default=1000)
     parser.add_argument("--batch_size", type=int, default=256)
     parser.add_argument("--memory_capacity", type=int, default=10000, help="Maximum number of memory replays to store.")
-    parser.add_argument("--pre_train_eps", type=int, default=100, help="Number of episodes to run before starting training. Allows memory to be built up.")
+    parser.add_argument("--pre_train_eps", type=int, default=10, help="Number of episodes to run before starting training. Allows memory to be built up.")
     # Model arguments
     args = parser.parse_args()
 
@@ -21,9 +22,12 @@ if __name__ == "__main__":
     algo = MADDPG(args.num_agents, env.get_observation_size(), env.dimensions, args.batch_size,
                  args.memory_capacity, args.pre_train_eps)
 
-    state = env.reset()
-    done = False
-    while(not done):
-        actions = algo.select_action(state)
-        _, _, state, done = env.step(actions.detach().cpu().numpy())
-    env.display_last_episode()
+    while (algo.episode_done < args.num_episodes):
+        print(algo.episode_done, "/", args.num_episodes)
+        state = env.reset()
+        done = False
+        while(not done):
+            actions = algo.select_action(state)
+            _, _, state, done = env.step(actions.detach().cpu().numpy())
+        algo.episode_done += 1
+        #env.display_last_episode()
